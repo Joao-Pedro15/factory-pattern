d81845b6cee47295e6bc7bca94123d31f182be97
João Pedro <jjoao.monteiro15@gmail.com>
2023-01-25 21:47:44 -0300

    implements fake database in memory

--- src/factory/UserFactory.js
+++ src/factory/UserFactory.js
@@ -1,9 +1,12 @@
 const UserRepository = require('../repository/UserRepository')
 const UserService = require('../service/UserService')
+const Database = require('../utils/Database')
 
 class UserFactory {
   async build() {
-    const userRepository = new UserRepository({ dbConnection: 'mongo://localhost' })
+    const db = new Database({ connectionString: 'mongodb://localhost' })
+    const dbConnection = await db.connect()
+    const userRepository = new UserRepository({ dbConnection })
     const userService = new UserService({ userRepository })
     return userService
   }
